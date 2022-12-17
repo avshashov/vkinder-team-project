ae347627a0662ef4609f748aa699e55f61928ce0
Artem Shashov <tauquantum@gmail.com>
2022-12-17 14:18:25 +1000

    added return_pair and download_pair

--- vk/vkbot.py
+++ vk/vkbot.py
@@ -16,18 +16,22 @@ class VkBot:
 
     def bot_session(self):
         longpoll = VkLongPoll(self.vk_session)
-
+        self._download_pairs(108062987) # убрать в функцию search_params()
         try:
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                     if event.text.lower() in ('старт', 'start', 'начать'):
                         self._send_message(event.user_id, 'Привет! Я бот для знакомств :)')
-                        vk = VKInfo(user_id=event.user_id, access_token=self.service_token)
-                        user_data = vk.get_user_info()
-                        user_photo = vk.get_photos()
+                        # vk = VKInfo(user_id=event.user_id, access_token=self.service_token)
+                        # vk = VKInfo(user_id=1670753, access_token=self.service_token)
+                        # user_data = vk.get_user_info()
+                        # user_photo = vk.get_photos()
+                        #
+                        # db = VkinderDB(self.user_db, self.password_db)
+                        # db.add_new_user(user_data, user_photo)
 
-                        db = VkinderDB(self.user_db, self.password_db)
-                        db.add_new_user(user_data, user_photo)
+                        self._return_pair(event.user_id)
+
 
         except Exception as ex:
             print(ex)
@@ -42,6 +46,23 @@ class VkBot:
         if not VkinderDB(self.user_db, self.password_db).search_params_exists(user_id):
             pass
         return
+
+    def _return_pair(self, user_id):
+        try:
+            pair = next(self.pair_iter)
+            print(pair)
+            # return pair
+        except StopIteration:
+            self._download_pairs(user_id)
+            pair = next(self.pair_iter)
+            print(pair)
+            # return pair
+
+    def _download_pairs(self, user_id):
+        # Функция должна быть внутри search_params()
+        db = VkinderDB(self.user_db, self.password_db)
+        self.pairs = db.find_a_couple(user_id)
+        self.pair_iter = iter(self.pairs)
 
 
 bot = VkBot(token_group, service_key, user_db, password_db)
